b562962ce8c26b82266a7f1e34060369fe887f2f
DJ <wadhwadhananjay@gmail.com>
2019-08-11 01:34:49 -0700

    Update hw5tests.rkt

--- hw5/hw5tests.rkt
+++ hw5/hw5tests.rkt
@@ -9,22 +9,27 @@
 
 (require rackunit)
 
+(define rlist (list (int 1) (int 2) (int 3)))
+(define mlist (apair (int 1) (apair (int 2) (apair (int 3) (munit)))))
+
 (define tests
   (test-suite
    "Homework 5 Tests"
-
+   
+   (check-equal? (racketlist->mupllist rlist) mlist)
+   (check-equal? (mupllist->racketlist mlist) rlist)
    (check-equal? (eval-exp (add (int 2) (int 2))) (int 4) "add simple test")
-
-   (check-exn (lambda (x) (string=? (exn-message x) "MUPL addition applied to non-number"))
-              (lambda () (eval-exp (add (int 2) (munit))))
-              "add bad argument")
-
-   (check-equal? (mupllist->racketlist
-                  (eval-exp (call (call mupl-all-gt (int 9))
-                                  (racketlist->mupllist 
-                                   (list (int 10) (int 9) (int 15))))))
-                 (list (int 10) (int 15))
-                 "provided combined test using problems 1, 2, and 4")
+;
+;   (check-exn (lambda (x) (string=? (exn-message x) "MUPL addition applied to non-number"))
+;              (lambda () (eval-exp (add (int 2) (munit))))
+;              "add bad argument")
+;
+;   (check-equal? (mupllist->racketlist
+;                  (eval-exp (call (call mupl-all-gt (int 9))
+;                                  (racketlist->mupllist 
+;                                   (list (int 10) (int 9) (int 15))))))
+;                 (list (int 10) (int 15))
+;                 "provided combined test using problems 1, 2, and 4")
    ))
 
 (require rackunit/text-ui)
